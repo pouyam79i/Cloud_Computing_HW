dc71e810b3393de997eaafafbf977a7002739fd3
pouyam79i <pouyamohammadi@gmail.com>
2023-05-10 08:54:32 +0430

    debug on redis connection

--- HW2/step2/server/api/redis.go
+++ HW2/step2/server/api/redis.go
@@ -14,13 +14,13 @@ var client *redis.Client = nil
 
 func initialConnectionToRedis() error {
 	server_info, err := util.GetConfigs()
-	if err != nil{
+	if err != nil {
 		return err
 	}
 	client = redis.NewClient(&redis.Options{
-		Addr: server_info.REDIS_ADDR,
+		Addr:     server_info.REDIS_ADDR,
 		Password: "",
-		DB: 0,
+		DB:       0,
 	})
 	return nil
 }
@@ -29,15 +29,18 @@ func initialConnectionToRedis() error {
 func GetRedis(key string) (string, error) {
 	if client == nil {
 		err := initialConnectionToRedis()
-		if err != nil{
-			return "" , err
+		if err != nil {
+			return "", err
 		}
 	}
 	if client == nil {
+		return "", errors.New("nil redis client")
+	}
+	if err := client.Ping(); err != nil {
 		return "", errors.New("no redis client connection")
 	}
 	data := client.Get(key)
-	if errors.Is(data.Err(), redis.Nil){
+	if errors.Is(data.Err(), redis.Nil) {
 		return "", redis.Nil
 	}
 	return data.Val(), nil
@@ -47,11 +50,14 @@ func GetRedis(key string) (string, error) {
 func SendRedis(key, val string) error {
 	if client == nil {
 		err := initialConnectionToRedis()
-		if err != nil{
+		if err != nil {
 			return err
 		}
 	}
 	if client == nil {
+		return errors.New("nil redis client")
+	}
+	if err := client.Ping(); err != nil {
 		return errors.New("no redis client connection")
 	}
 	exp := time.Duration(300 * time.Second) // 5 minutes
